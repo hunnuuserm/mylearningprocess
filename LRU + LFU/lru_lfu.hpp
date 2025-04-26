95624360f259cb6a4a6d00d2a98e3f1025b4ec6f
hunnuuserm <3072764440@qq.com>
2025-04-26 16:32:32 +0800

    update LRU

--- LRU + LFU/lru_lfu.hpp
+++ LRU + LFU/lru_lfu.hpp
@@ -3,80 +3,77 @@
 
 using namespace std;
 
-class LRUCache {
+// LRU注意点:
+// 1.put的时候，要检查是否存在，存在就把_list和_hash中已存在的删掉(_list一定要删，_hash可以不删，后面直接_hash[key]更新迭代器也行)
+//   如果不删除_list中已存在的数据，_list中就会存在两份{key, vlaue1}、{key, value2}了，当然value1和value2可能相等可能不相等;
+// 2.put的时候记的检查个数是否达到上线了，即_list.size()是否等于_capacity;
+// 3.不论是put还是get，_list和_hash都要一起更新，不要只更新其中一个。
+class LRUCache
+{
 public:
-	LRUCache(int capacity) {
-		_capacity = capacity;
+	LRUCache(int capacity)
+		: _capacity(capacity)
+	{
 	}
-	int get(int key) {
-			auto hashit = _hashmap.find(key);
-		if (hashit != _hashmap.end())
-		{
-			auto listit = hashit->second;
-			pair<int, int> kv = *listit;
 
-			_list.erase(listit);
-			_list.push_front(kv);
-			_hashmap[key] = _list.begin();
-			return kv.second;
-		}
-		else
-		{
+	int get(int key)
+	{
+		auto it = _hash.find(key);
+		if (it == _hash.end())
 			return -1;
-		}
-	}
-	void put(int key, int value) {
-		// 1.如果没有数据则进行插入数据
-		// 2.如果有数据则进行数据更新
-		auto hashit = _hashmap.find(key);
-		if (hashit == _hashmap.end())
-		{
-			//比特就业课
-				// 插入数据时，如果数据已经达到上限，则删除链表头的数据和hashmap中的数据，两个
-				//删除操作都是O(1)
-				if (_list.size() >= _capacity)
-				{
-					_hashmap.erase(_list.back().first);
-					_list.pop_back();
-				}
 
-			_list.push_front(make_pair(key, value));
-			_hashmap[key] = _list.begin();
-		}
-		else
-		{
-			// 再次put，将数据挪动list前面
-			auto listit = hashit->second;
-			pair<int, int> kv = *listit;
-			kv.second = value;
-
-			_list.erase(listit);
-			_list.push_front(kv);
-			_hashmap[key] = _list.begin();
-		}
+		int value = it->second->second;
+		_list.erase(it->second);
+		_list.push_front({key, value});
+		_hash[key] = _list.begin();
+		return value;
 	}
+
+	void put(int key, int value)
+	{
+		auto it = _hash.find(key);
+		if (it != _hash.end())
+		{
+			_list.erase(it->second);
+			_hash.erase(it);
+		}
+		else if (_list.size() == _capacity)
+		{
+			int k = _list.back().first;
+			_list.pop_back();
+			_hash.erase(k);
+		}
+
+		_list.push_front({key, value});
+		_hash[key] = _list.begin();
+	}
+
 private:
 	list<pair<int, int>> _list;
-	size_t _capacity;
-	unordered_map<int, list<pair<int, int>>::iterator> _hashmap;
+	unordered_map<int, list<pair<int, int>>::iterator> _hash;
+	int _capacity;
 };
 
-class LFUCache {
+class LFUCache
+{
 private:
 	int capacity;
 	int minFreq;
-	unordered_map<int, int> cache; // 存储键值对
-	unordered_map<int, int> freq;  // 存储键的访问频率
+	unordered_map<int, int> cache;			// 存储键值对
+	unordered_map<int, int> freq;			// 存储键的访问频率
 	unordered_map<int, list<int>> freqList; // 存储频率和访问该频率的键的双向链表
 
 public:
-	LFUCache(int capacity) {
+	LFUCache(int capacity)
+	{
 		this->capacity = capacity;
 		this->minFreq = 0;
 	}
 
-	int get(int key) {
-		if (cache.find(key) == cache.end()) {
+	int get(int key)
+	{
+		if (cache.find(key) == cache.end())
+		{
 			return -1; // 缓存中没有该键
 		}
 
@@ -86,7 +83,8 @@ public:
 
 		// 将键从当前频率链表移除
 		freqList[currentFreq].remove(key);
-		if (freqList[currentFreq].empty() && currentFreq == minFreq) {
+		if (freqList[currentFreq].empty() && currentFreq == minFreq)
+		{
 			minFreq++;
 		}
 
@@ -96,16 +94,20 @@ public:
 		return cache[key];
 	}
 
-	void put(int key, int value) {
-		if (capacity == 0) return;
+	void put(int key, int value)
+	{
+		if (capacity == 0)
+			return;
 
-		if (cache.find(key) != cache.end()) {
+		if (cache.find(key) != cache.end())
+		{
 			cache[key] = value;
-			get(key);  // 更新频率
+			get(key); // 更新频率
 			return;
 		}
 
-		if (cache.size() >= capacity) {
+		if (cache.size() >= capacity)
+		{
 			// 淘汰最不常用的项
 			int leastFreqKey = freqList[minFreq].front();
 			freqList[minFreq].pop_front();
